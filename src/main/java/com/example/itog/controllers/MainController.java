b3fe34f7c29e83ddc06b935cb7970b9d324b3760
gero-web <sergeq198@yandex.ru>
2023-01-29 22:49:23 +0800

    controller crud

--- src/main/java/com/example/itog/controllers/MainController.java
+++ src/main/java/com/example/itog/controllers/MainController.java
@@ -2,14 +2,44 @@ package com.example.itog.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import com.example.itog.models.PledgeTicket;
+import com.example.itog.service.PledgeTicketServiceImp;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 public class MainController {
 
-    @GetMapping("/index")
-    public @ResponseBody String getText() {
-        return "asdsadda";
+    @Autowired
+    private PledgeTicketServiceImp rep;
+
+    @PostMapping("/create")
+    public PledgeTicket saveDepartment(
+            @Valid @RequestBody PledgeTicket pledgeTicket) {
+        return rep.savePledgeTicket(pledgeTicket);
     }
+
+    @GetMapping("/index")
+    public List<PledgeTicket> getText() {
+        return rep.fetchPledgeTicketList();
+    }
+
+    @PutMapping("/updates/{id}")
+    public PledgeTicket updateDepartment(@RequestBody PledgeTicket pledgeTicket,
+            @PathVariable("id") Integer pledgeTicketId) {
+        return rep.updateDPledgeTicket(
+                pledgeTicket, pledgeTicketId);
+    }
+
+    @DeleteMapping("/departments/{id}")
+    public String deleteDepartmentById(@PathVariable("id") Integer id) {
+        rep.deletePledgeTicketById(
+                id);
+        return "Deleted Successfully";
+    }
+
 }
